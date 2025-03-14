795b1a267819dfb705a9811a1c25f46e66684ef4
Alex45121 <105495005+Alex45121@users.noreply.github.com>
2025-03-14 15:51:00 +0100

    Review_checker_it1

--- Review_checker.py
+++ Review_checker.py
@@ -3,24 +3,73 @@ import re
 class BoWReview_checker():
     def __init__(self):
 
-        self.good_words = ["amazing", "good", "loved","fantastic"]
-        self.bad_words = ["awful", "bad", "hated","disgusting"]
+        self.good_words = ["amazing", "good", "loved","fantastic","nice","liked"]
+        self.bad_words = ["awful", "bad", "hated","disgusting", "didn'", "shit"]
+        self.words = []
+        self.new_dic = {}
+
  
 
     def _text_analyzer(self, text):
+        if not isinstance(text, str) or not text.strip():
+            raise ValueError("Please use words!")
         self.words = re.findall(r"\b\w+\b", text.lower())
         return self.words
     
     def _counter(self):
-        index = 0
+    
         self.new_dic = {}
         for token in self.words:
-            if token not in self.words:
-                self.new_dic[token] = index
-                index += 1
-        print (self.new_dic)
+            if token in self.new_dic:
+                self.new_dic[token] += 1
+            else:
+                self.new_dic[token] = 1
+                
+        return self.new_dic
+    
+    def __calculator(self):
+        good_count = 0
+        bad_count = 0
+        
+        for keys, values in self.new_dic.items():
+            if keys in self.good_words:
+                good_count += values
+                
+            elif keys in self.bad_words:
+                bad_count += values
+        if good_count > bad_count:
+            return True
+        elif bad_count > good_count:
+            return False
+        else:
+            return None
+           
+            
+    def _assumption(self):
+        result = self.__calculator()
+        if result is True:
+            print("The review was Positive")
+        elif result is False:
+            print("The review was Negative")
+        else:
+            print("The review was Neutral")
+
+if __name__ =="__main__" :
+
+    
+    try:
+        processor = BoWReview_checker()
+        review = input("Leave your review here: ")
+        processor._text_analyzer(review)
+        print(processor._counter())
+        print(processor._assumption())
+    except ValueError as e:
+        print(f"Unexpected error {e}")
 
 
-processor = BoWReview_checker()
-print(processor._text_analyzer("The movie was fantastic, I loved it!"),processor._counter)
+
+
+
+
+
 
